667e8642dcf036361ca61d3fa56c20afb5ed9329
Jin Soo Kim <skywlstn777@gmail.com>
2023-10-13 15:50:05 +0900

    #1
    간단한 테스트를 위한 회원가입 로그인 구현

---
+++ src/main/java/com/team5/WalkingWithWorld/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.team5.WalkingWithWorld.controller;
+
+import com.team5.WalkingWithWorld.domain.LoginDto;
+import com.team5.WalkingWithWorld.domain.Users;
+import com.team5.WalkingWithWorld.domain.UsersDto;
+import com.team5.WalkingWithWorld.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView loginUser(LoginDto loginDto,
+                                  HttpSession session){
+        ModelAndView modelAndView = new ModelAndView();
+        Users user = userService.getUserInfo(loginDto);
+
+        System.out.println("로그인 성공");
+
+        session.setAttribute("Authorization",user);
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @GetMapping("/signup")
+    public String index(){
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public ModelAndView signUpUser(UsersDto usersDto){
+        ModelAndView modelAndView = new ModelAndView();
+        List<UsersDto> userList = userService.getAllUsers();
+        userService.createUser(usersDto);
+
+        modelAndView.addObject("list", userList);
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+}
