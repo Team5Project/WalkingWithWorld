b75d6b46057cacab77cec14f5845299fe95efd11
Jin Soo Kim <skywlstn777@gmail.com>
2023-10-15 16:25:00 +0900

    #1 로그인 유저 확인을 위한 메인 페이지 구현 및 잡다한 것

--- src/main/java/com/team5/WalkingWithWorld/controller/UserController.java
+++ src/main/java/com/team5/WalkingWithWorld/controller/UserController.java
@@ -2,6 +2,7 @@ package com.team5.WalkingWithWorld.controller;
 
 import com.team5.WalkingWithWorld.domain.LoginDto;
 import com.team5.WalkingWithWorld.domain.UsersDto;
+import com.team5.WalkingWithWorld.global.Login;
 import com.team5.WalkingWithWorld.global.SessionConst;
 import com.team5.WalkingWithWorld.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,15 +27,26 @@ public class UserController {
 
 
     @GetMapping("/signup")
-    public String index(){
+    public String index() {
         return "signupForm";
     }
 
     @GetMapping("/login")
     public String loginIndex(@RequestParam(defaultValue = "/") String redirectURL,
-                                   Model model){
+                             Model model,
+                             HttpSession session) {
         model.addAttribute("redirectURL", redirectURL);
+
         return "loginForm";
+    }
+
+    @GetMapping("/logout")
+    public String logoutIndex(@Login UsersDto usersDto,
+                              HttpSession session) {
+        if (usersDto != null) {
+            session.invalidate();
+        }
+        return "";
     }
 
     @PostMapping("/login")
@@ -42,31 +54,31 @@ public class UserController {
                             HttpSession session,
                             BindingResult bindingResult,
                             HttpServletRequest requet
-                             ){
+    ) {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "loginForm";
         }
 
         UsersDto user = userService.getUserInfo(loginDto);
-        if(user == null){
-            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
+        if (user == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             System.out.println("아이디 또는 패스워드 오류");
             return "loginForm";
         }
 
-        session.setAttribute(SessionConst.LONGIN_USERS,user);
+        session.setAttribute(SessionConst.LONGIN_USERS, user);
+        String redirectURL = requet.getParameter("redirectURL");
 
-        if(requet.getParameter("redirectURL") != null){
-            String redirectURL = requet.getParameter("redirectURL");
+        if (redirectURL != null && !redirectURL.equals("/logout")) {
             System.out.println("리다이렉트 확인" + redirectURL);
-            return "redirect:"+redirectURL;
+            return "redirect:" + redirectURL;
         }
         return "redirect:/";
     }
 
     @PostMapping("/signup")
-    public String signUpUser(UsersDto usersDto){
+    public String signUpUser(UsersDto usersDto) {
         List<UsersDto> userList = userService.getAllUsers();
         boolean user = userService.createUser(usersDto);
         System.out.println(user);
