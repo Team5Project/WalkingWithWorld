3d407b53ffc1c284926bd24f467fb39d92d67f1a
Frog <84081437+huiji-park@users.noreply.github.com>
2023-10-17 18:22:26 +0900

    #24 게시글 수정

--- src/main/java/com/team5/WalkingWithWorld/controller/WalkingPathsController.java
+++ src/main/java/com/team5/WalkingWithWorld/controller/WalkingPathsController.java
@@ -9,10 +9,7 @@ import com.team5.WalkingWithWorld.service.WalkingPathService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
@@ -61,20 +58,26 @@ public class WalkingPathsController {
     public ModelAndView goToWrite(HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
         mav.addObject("referer", request.getHeader("referer"));
-        mav.setViewName("walking-path_form");
+        mav.setViewName("walking-path_create_form");
         return mav;
     }
     @GetMapping("/walking-path/modify/{walking-path-id}")
     public ModelAndView goToModify(@PathVariable("walking-path-id") int walkingPathId, HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
         mav.addObject("referer", request.getHeader("referer"));
-        mav.addObject("walkingPathId", walkingPathId);
-        mav.setViewName("walking-path_form");
+        mav.addObject("walkingPath", dao.readWalkingPath(walkingPathId));
+        mav.setViewName("walking-path_modify_form");
         return mav;
     }
-
+    @PostMapping("/walking-path/modify")
+    public String modifyWalkingPath(WalkingPathsDTO walkingPathsDTO, @Login UsersDto loginUser) {
+        walkingPathsDTO.setModifiedBy(loginUser.getName());
+        walkingPathsDTO.setId(walkingPathsDTO.getId());
+        int result = dao.updateWalkingPath(walkingPathsDTO);
+        System.out.println("수정 결과 : " + result);
+        return "redirect:/walking-path";
+    }
     @GetMapping("/walking-path/{walking-path-id}")
-    @ResponseBody
     public ModelAndView getWalkingPathById(@PathVariable("walking-path-id") int id, HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
         WalkingPathsMapDTO walkingPaths = walkingPathService.readWalkingPathById(id);
@@ -87,7 +90,11 @@ public class WalkingPathsController {
         mav.setViewName("walking-path_detail");
         return mav;
     }
-
+    @GetMapping("/walking-path/detail/{walking-path-id}")
+    @ResponseBody
+    public WalkingPathsMapDTO getWalkingPathMapById(@PathVariable("walking-path-id") int id) {
+        return dao.readWalkingPath(id);
+    }
     @GetMapping("/walking-path/photos/{walking-path-id}")
     @ResponseBody
     public List<PhotosDTO> getPhotosByWalkingPathId(@PathVariable("walking-path-id") int id) {
--- src/main/java/com/team5/WalkingWithWorld/dao/WalkingPathsMapper.java
+++ src/main/java/com/team5/WalkingWithWorld/dao/WalkingPathsMapper.java
@@ -8,14 +8,17 @@ import java.util.List;
 
 @Mapper
 public interface WalkingPathsMapper {
-    @Select("select id, users_id, title, addr, created_at, created_by from walking_paths")
+    @Select("select id, users_id, title, addr, created_at, created_by, modified_at, modified_by from walking_paths order by created_at desc")
     List<WalkingPathsDTO> readAll();
     @Insert("Insert into walking_paths (users_id, title, addr, created_at, created_by) values (#{usersId}, #{title}, #{addr}, now(), #{createdBy})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     int addWalkingPath(WalkingPathsDTO dto);
 
-    @Select("select id, users_id, title, addr, created_at, created_by from walking_paths where id = #{walkingPathId}")
+    @Select("select id, users_id, title, addr, created_at, created_by, modified_at, modified_by from walking_paths where id = #{walkingPathId}")
     WalkingPathsMapDTO readWalkingPath(int WalkingPathId);
+
+    @Update("update walking_paths set title = #{title}, addr = #{addr}, modified_at = now(), modified_by = #{modifiedBy} where id = #{id}")
+    int updateWalkingPath(WalkingPathsDTO dto);
 
     @Delete("delete from walking_paths where id = #{walkingPathId}")
     int deleteWalkingPath(int WalkingPathId);
