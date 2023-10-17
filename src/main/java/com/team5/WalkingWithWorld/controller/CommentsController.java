66b4190d808729809aa3afb5723b3ac3d2fa1702
Jin Soo Kim <skywlstn777@gmail.com>
2023-10-17 18:24:11 +0900

    #backup 코멘트 수정

--- src/main/java/com/team5/WalkingWithWorld/controller/CommentsController.java
+++ src/main/java/com/team5/WalkingWithWorld/controller/CommentsController.java
@@ -2,6 +2,9 @@ package com.team5.WalkingWithWorld.controller;
 
 import com.team5.WalkingWithWorld.dao.CommentsMapper;
 import com.team5.WalkingWithWorld.domain.CommentsDTO;
+import com.team5.WalkingWithWorld.domain.UsersDto;
+import com.team5.WalkingWithWorld.global.Login;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,14 +25,21 @@ public class CommentsController {
         return "comments :: #comments";
     }
 
-    @PostMapping("/comments/{walking-paths-id}")
-    public ModelAndView write(CommentsDTO dto,
-                              @PathVariable("walking-paths-id") int id){
-        boolean result = dao.write(dto);
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("list",dao.list());
-        mav.setViewName("redirect:/comment");
-        return mav;
+    @PostMapping(value = "/comments/{walking-paths-id}",produces = "application/json; charset=utf-8")
+    public String writeComment(@RequestBody CommentsDTO dto,
+                               @Login UsersDto usersDto,
+                               @PathVariable("walking-paths-id") int id,
+                               Model model,
+                               HttpServletRequest request) {
+        String ref = request.getHeader("Referer");
+
+        dto.setUsers_id(usersDto.getId());
+        dto.setWalking_paths_id(id);
+        dao.write(dto);
+
+        List<CommentsDTO> list = dao.list();
+        model.addAttribute("commentList", list);
+        return "comments :: #comments";
     }
 
     @RequestMapping(value = "/comments/updateComments", produces = "application/json; charset=utf-8")
@@ -39,19 +49,19 @@ public class CommentsController {
     }
 
     @RequestMapping("/comments/update")
-    public ModelAndView update(CommentsDTO dto){
+    public ModelAndView update(CommentsDTO dto) {
         boolean result = dao.update(dto);
         ModelAndView mav = new ModelAndView();
-        mav.addObject("list",dao.list());
+        mav.addObject("list", dao.list());
         mav.setViewName("redirect:/comment");
         return mav;
     }
 
     @RequestMapping("/comments/delete")
-    public ModelAndView delete(String id){
+    public ModelAndView delete(String id) {
         boolean result = dao.delete(id);
         ModelAndView mav = new ModelAndView();
-        mav.addObject("list",dao.list());
+        mav.addObject("list", dao.list());
         mav.setViewName("redirect:/comment");
         return mav;
     }
