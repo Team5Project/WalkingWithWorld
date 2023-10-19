482747d1b7230a2903a4ae8561c031372e95b69d
Frog <84081437+huiji-park@users.noreply.github.com>
2023-10-19 17:49:23 +0900

    #37 게시글 조회-map 정보 추가

--- src/main/java/com/team5/WalkingWithWorld/domain/CommentsDTO.java
+++ src/main/java/com/team5/WalkingWithWorld/domain/CommentsDTO.java
@@ -1,5 +1,6 @@
 package com.team5.WalkingWithWorld.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,8 @@ public class CommentsDTO  {
     private int walking_paths_id;
     private int users_id;
     private String name;
+
+    @NotBlank(message = "댓글을 입력해주세요.")
     private String content;
     private LocalDateTime created_at;
     private LocalDateTime modified_at;
