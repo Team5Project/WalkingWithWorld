8524867807ebdb418aac873d8b7888f5347fd190
vaem6877 <119560458+vaem6877@users.noreply.github.com>
2023-10-31 18:46:09 +0900

    파일 생성

--- src/main/java/com/team5/WalkingWithWorld/entity/Comments.java
+++ src/main/java/com/team5/WalkingWithWorld/entity/Comments.java
@@ -1,4 +1,28 @@
 package com.team5.WalkingWithWorld.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
 public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer walkingPathsId;
+    private Integer users_id;
+    private String name;
+
+    @NotBlank(message = "댓글을 입력해주세요.")
+    private String content;
+    private java.sql.Date created_at;
+    private java.sql.Date modified_at;
 }
---
+++ src/test/java/com/team5/WalkingWithWorld/repository/CommentsRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.team5.WalkingWithWorld.repository;
+
+import com.team5.WalkingWithWorld.entity.Comments;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class CommentsRepositoryTest {
+    @Autowired
+    private CommentsRepository cr;
+
+    @Test
+    void findAll(){
+        List<Comments> list = cr.findAll();
+        list.stream().forEach(System.out::println);
+    }
+}
