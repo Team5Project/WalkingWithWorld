dd34152b008e8156a3464badde836589c90851cc
Yoon sangHeyok <119560458+vaem6877@users.noreply.github.com>
2023-10-31 22:23:48 +0900

    인서트JPA테스트중

--- src/main/java/com/team5/WalkingWithWorld/repository/CommentsRepository.java
+++ src/main/java/com/team5/WalkingWithWorld/repository/CommentsRepository.java
@@ -1,8 +1,13 @@
 package com.team5.WalkingWithWorld.repository;
 
 
+import com.team5.WalkingWithWorld.domain.CommentsDTO;
 import com.team5.WalkingWithWorld.entity.Comments;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentsRepository extends JpaRepository<Comments,Integer> {
+    Comments save(Comments comments);
 }
