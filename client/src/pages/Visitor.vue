9ba3ec1e6169eebd0622936e2ce6e4d2184e474c
leesl123 <elegarden89@gmail.com>
2023-11-15 17:35:36 +0900

    #71-visitors-2
    뷰 전환중

--- client/src/pages/Visitor.vue
+++ client/src/pages/Visitor.vue
@@ -8,7 +8,7 @@
     <section>
       <div id="article">
         <article id="deleteList">
-          <div class="vwrapper" v-for="vo in list" :key="vo.id">
+          <div class="vwrapper" v-for="vo in list.data">
             <div class="visit_header">
               <span>작성자</span>
               <span>내용</span>
@@ -36,9 +36,7 @@
         </article>
       </div>
       <div class="btn_write_wrapper">
-        <a
-          th:href="@{/insertVisitorsForm}"
-          class="visitor_write btns btn_vwrite"
+        <a href="@{/insertVisitorsForm}" class="visitor_write btns btn_vwrite"
           >글쓰기</a
         >
       </div>
@@ -48,35 +46,23 @@
   <Footer />
 </template>
 
-<script>
+<script setup>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import { ref } from "vue";
+import axios from "axios";
 
-export default {
-  components: {
-    Header,
-    Footer,
-  },
-
-  methods: {
-    deleteVisitorList: function () {
-      var visitorsId = id;
-      var VisitorsDTO = {};
-      VisitorsDTO.id = id;
-
-      const password = prompt("비밀번호를 입력하세요:");
-      VisitorsDTO.password = password;
-      axios
-        .delete("http:/localhost:8089/{id}")
-        .then((response) => {
-          return response.data;
-        })
-        .then((data) => {
-          document.getElementById("article").innerHTML = `${data}`;
-        });
-    },
-  },
+const list = ref([]);
+const fetchList = async () => {
+  const response = await axios.get("http://localhost : 8089/visitors");
+  return response.data;
 };
+const setList = async () => {
+  list.value = await fetchList();
+};
+setList().then(() => {
+  console.log(list.value.data);
+});
 </script>
 <style scoped>
 @import "@/assets/visitor.css";
